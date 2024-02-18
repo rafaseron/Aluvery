7914264c0f48b207c174f61c7b9a1cfe2a659701
rafaseron <rafaelseron3011@gmail.com>
2024-02-18 18:05:40 -0400

    ProductItem

--- app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.rafaseron.aluvery.ui.theme.AluveryTheme
@@ -30,92 +32,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    //MyFirstComposable()
-                    CustomLayoutPreview()
+                    ProductItem()
                 }
             }
         }
     }
 }
 
-@Preview (showBackground = true)
+@Preview(showBackground = true/*, showSystemUi = true*/)
 @Composable
-fun ColumnPreview() {
-    Column(
-        Modifier
-            .background(color = Color.Magenta)
-            .padding(all = 8.dp)) {
-        Text(text = "My First Composable")
-        Text(text = "Texto maior bem maior ta ligado")
+fun ProductItem() {
+    Column() {
+        Box(modifier = Modifier
+            .height(50.dp)
+            .width(50.dp)
+            .background(color = Color.Blue))
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Imagem do Produto")
+        Text(text = "Nome do produto")
+        Text(text = "Preço")
     }
 }
 
-@Preview (showBackground = true)
-@Composable
-fun RowPreview() {
-    Row {
-        Text(text = "My First Composable")
-        Text(text = "Texto maior bem maior ta ligado")
-    }
-}
-
-@Preview (showBackground = true)
-@Composable
-fun BowPreview() {
-    Box {
-        Text(text = "My First Composable")
-        Text(text = "Texto maior bem maior ta ligado")
-    }
-}
-
-@Preview (showBackground = true, showSystemUi = true)
-@Composable
-fun CustomLayoutPreview() {
-    Column(
-        Modifier
-            .padding(all = 10.dp)
-            .background(color = Color.Blue)
-            //.height(400.dp)
-            //.width(200.dp)
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .fillMaxSize()) {
-        Text(text = "Rafael Seron")
-        Text(text = "Android Developer")
-        Row(
-            Modifier
-                .padding(10.dp)
-                .background(color = Color.Red)
-                .fillMaxWidth()) {
-            Text(text = "RS")
-            Text(text = "✅")
-        }
-        Box(
-            Modifier
-                .background(color = Color.Yellow)
-                .fillMaxWidth(fraction = 0.5f)){ /*dessa forma ele
-            ocupa 50% da fracao de tela que ele tem "disponivel"*/
-            Text(text = "✅")
-            Text(text = "Texto 6")
-        }
-    }
 
 
-}
-
-@Composable
-fun MyFirstComposable(){
-    Text(text = "My First Composable")
-    Text(text = "Texto maior bem maior ta ligado")
-}
-
-@Preview
-@Composable
-fun MyFirstComposablePreview(){
-    AluveryTheme {
-        Surface {
-            MyFirstComposable()
-        }
-    }
-
-}
