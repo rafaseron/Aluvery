35d9f7d1d138603af50181aa5ae1a3111b865849
rafaseron <rafaelseron3011@gmail.com>
2024-02-19 19:11:43 -0400

    ProductItem

--- app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,14 +14,23 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,15 +52,32 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true/*, showSystemUi = true*/)
 @Composable
 fun ProductItem() {
-    Column() {
+    val purple = colorResource(id = R.color.purple_500)
+    val listColors = listOf<Color>(purple, Color.Cyan)
+    Column(modifier = Modifier
+        .height(250.dp)
+        .width(200.dp)
+        .clip(RoundedCornerShape(15.dp))
+        .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(15.dp))) {
         Box(modifier = Modifier
-            .height(50.dp)
-            .width(50.dp)
-            .background(color = Color.Blue))
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Imagem do Produto")
-        Text(text = "Nome do produto")
-        Text(text = "Preço")
+            .height(100.dp)
+            .fillMaxWidth()
+            .background(brush = Brush.horizontalGradient(colors = listColors)))
+
+        Box(modifier = Modifier.padding(horizontal = 50.dp)){
+            Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Imagem do Produto",
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    /*.size(100.dp) da pra ser assim tbm o tamanho da imagem*/
+                    .offset(y = -50.dp)
+                    .clip(shape = CircleShape))
+        }
+        Text(text = "Nome do produto", modifier = Modifier.padding(horizontal = 20.dp,
+            vertical = 2.dp).offset(y = -30.dp))
+        Text(text = "Preço", modifier = Modifier.padding(horizontal = 20.dp,
+            vertical = 2.dp).offset(y = -25.dp))
     }
 }
 
