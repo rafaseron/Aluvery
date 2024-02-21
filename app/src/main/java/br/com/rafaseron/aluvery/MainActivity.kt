b0321dbcffba11e78c9360313f8a690e5f4e0d8d
rafaseron <rafaelseron3011@gmail.com>
2024-02-21 12:08:02 -0400

    Row horizontalScroll

--- app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
@@ -6,8 +6,10 @@ import android.transition.CircularPropagation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,6 +24,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -70,10 +73,15 @@ fun ProductSection () {
             modifier = Modifier.padding(start = 16.dp, top = 8.dp))
 
 
-        Row(modifier = Modifier.padding(start = 16.dp,
-            end = 16.dp,
-            bottom = 8.dp)
-            .fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        Row(modifier = Modifier
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 8.dp
+            )
+            .fillMaxWidth()
+            .horizontalScroll(state = rememberScrollState() /*ScrollState(0)*/),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
             ProductItem()
             ProductItem()
             ProductItem()
