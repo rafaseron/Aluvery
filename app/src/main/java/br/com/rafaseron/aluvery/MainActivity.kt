56c7d4c073f79d8d9efcff20ff6675fd76e39cfa
rafaseron <rafaelseron3011@gmail.com>
2024-02-22 00:56:34 -0400

    Retirar imports desnecessarios

--- app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/rafaseron/aluvery/MainActivity.kt
@@ -3,42 +3,10 @@ package br.com.rafaseron.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.com.rafaseron.aluvery.model.Product
-import br.com.rafaseron.aluvery.ui.components.ProductItem
-import br.com.rafaseron.aluvery.ui.components.ProductSection
+import br.com.rafaseron.aluvery.ui.screens.HomeScreen
 import br.com.rafaseron.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,16 +26,7 @@ class MainActivity : ComponentActivity() {
 fun App() {
     AluveryTheme {
         Surface {
-            Spacer(modifier = Modifier)
-            Column(verticalArrangement = Arrangement.spacedBy(16.dp), modifier = Modifier
-                .fillMaxSize(1f)
-                .verticalScroll(rememberScrollState())) {
-                Spacer(modifier = Modifier)
-                ProductSection()
-                ProductSection()
-                ProductSection()
-                Spacer(modifier = Modifier)
-            }
+            HomeScreen()
         }
     }
 
@@ -77,7 +36,6 @@ fun App() {
 @Composable
 fun AppPreview() {
     App()
-    
 }
 
 
---
+++ app/src/main/java/br/com/rafaseron/aluvery/ui/screens/HomeScreen.kt
@@ -0,0 +1,32 @@
+package br.com.rafaseron.aluvery.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.rafaseron.aluvery.ui.components.ProductSection
+
+@Composable
+fun HomeScreen() {
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp), modifier = Modifier
+        .fillMaxSize(1f)
+        .verticalScroll(rememberScrollState())) {
+        Spacer(modifier = Modifier)
+        ProductSection()
+        ProductSection()
+        ProductSection()
+        Spacer(modifier = Modifier)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
